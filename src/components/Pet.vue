28801164c1577c1f44af88121c2e709f2159dda1
Asheton S. M. <98793112+ashetonsm@users.noreply.github.com>
2025-08-07 18:57:17 -0500

    Created a simple placeholder for pet stats.

---
+++ src/components/Pet.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  pet: { name: string, hunger: string, mood: string, image: string }
+}>()
+</script>
+
+<template>
+  <div class="pet-container">
+
+    <div class="pet-image">
+        <img :src="'/src/assets/testPets/' + pet.image + '.jpg'">
+        <!-- <img src="@/assets/testPets/1.jpg"> -->
+         <!-- For some reason, we can't use a "@" alias here for our string literal -->
+        {{ "an image of " + pet.name }}
+    </img>
+</div>
+
+    <div class="pet-info">
+        <h1 class="green">{{ pet.name }}</h1>
+        <h2>hunger:</h2>
+        <h2 class="green">{{ pet.hunger }}</h2>
+        <h2 >mood:</h2>
+        <h3 class="green">{{ pet.mood }}</h3>
+    </div>
+  </div>
+</template>
