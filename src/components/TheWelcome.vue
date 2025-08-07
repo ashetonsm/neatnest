58d7a130b75d9f3cf8b1c0d0c8a3cb6f15e0232a
Asheton S. M. <98793112+ashetonsm@users.noreply.github.com>
2025-08-07 18:58:10 -0500

    Used for testing pet info.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,50 +1,22 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import Pet from './Pet.vue'
 
-const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+const pets = [
+  { name: 'samia', hunger: 'full', mood: 'content', image: '1' },
+  { name: 'brae', hunger: 'starving', mood: 'happy', image: '2' }
+]
+
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/vuejs/language-tools" target="_blank" rel="noopener">Vue - Official</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a>
-    and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
-    /
-    <a href="https://playwright.dev/" target="_blank" rel="noopener">Playwright</a>.
-
-    <br />
-
-    More instructions are available in
-    <a href="javascript:void(0)" @click="openReadmeInEditor"><code>README.md</code></a
-    >.
-  </WelcomeItem>
-
+  <template v-if="!pets">
+    <h1>Loading!</h1>
+  </template>
+  <template v-else-if="!pets.length">
+    <h1>No pets found</h1>
+  </template>
+  <template v-else>
+    <Pet v-for="(pet, i) in pets" :key=pet.name :pet=pet />
+  </template>
 </template>
