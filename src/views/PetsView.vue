2416d71ea293efbb4bc32b79fb028c5ebd59b4d6
Asheton S. M. <98793112+ashetonsm@users.noreply.github.com>
2025-08-08 11:28:47 -0500

    Created a page for viewing user's pets.

---
+++ src/views/PetsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PetDisplay from '@/components/PetDisplay.vue';
+
+</script>
+
+<template>
+  <div class="page" id="petsPage">
+    <PetDisplay/>
+  </div>
+</template>
