580ac21d56be6616b9032b786013a4dc2c126ce5
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-08-08 19:27:50 -0500

    Added some test types and more test item data.

---
+++ src/assets/ItemExports.ts
@@ -0,0 +1,12 @@
+export type ItemType = {
+    itemid: string;
+    name: string;
+    price: string;
+    shopfront: string;
+    owner: string;
+    health: string;
+    rarity: string;
+    image: string;
+}
+
+export type ItemArray = ItemType[]
---
+++ src/assets/testData/itemObjectList.ts
@@ -0,0 +1,64 @@
+import type { ItemArray } from "../ItemExports";
+
+export const testItems:ItemArray = [
+    {
+        itemid: '0', 
+        name: 'Rubber Duck', 
+        price: '5', 
+        shopfront: 'Test Emporium',
+        owner: 'nnneato', 
+        health: '100', 
+        rarity: '0', 
+        image: 'rubberduck'
+    },
+    {
+        itemid: '1', 
+        name: 'Rainbow Cake', 
+        price: '55', 
+        shopfront: 'Test Emporium',
+        owner: 'nnneato', 
+        health: '100', 
+        rarity: '1', 
+        image: 'cake'
+    },
+    {
+        itemid: '2', 
+        name: 'White Shirt', 
+        price: '15', 
+        shopfront: 'Test Emporium',
+        owner: 'nnneato', 
+        health: '100', 
+        rarity: '0', 
+        image: 'whiteshirt'
+    },
+    {
+        itemid: '3', 
+        name: 'Stuffed Bunny', 
+        price: '555', 
+        shopfront: 'Test Emporium',
+        owner: '', 
+        health: '100', 
+        rarity: '10', 
+        image: 'stuffedbunny'
+    },
+    {
+        itemid: '4', 
+        name: 'Soccer Ball', 
+        price: '5', 
+        shopfront: 'Test Emporium',
+        owner: '', 
+        health: '100', 
+        rarity: '0', 
+        image: 'soccerball'
+    },
+    {
+        itemid: '5', 
+        name: 'Jack O Lanturn', 
+        price: '543', 
+        shopfront: 'Test Emporium',
+        owner: '', 
+        health: '50', 
+        rarity: '90', 
+        image: 'jackolanturn'
+    },
+]
