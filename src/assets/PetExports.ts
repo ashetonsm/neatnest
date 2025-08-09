70367851379c159a397219511937ec88eb1beb07
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-08-08 22:25:18 -0500

    New pet data

---
+++ src/assets/PetExports.ts
@@ -0,0 +1,11 @@
+export type PetType = {
+    petId: string;
+    name: string;
+    hunger: string;
+    mood: string;
+    owner: string;
+    health: string;
+    image: string;
+}
+
+export type PetArray = PetType[]
