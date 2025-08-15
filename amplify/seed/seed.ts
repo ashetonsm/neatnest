f53b9ce99630ce06198e1b46a4751c3c89593c08
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-08-14 23:31:46 -0500

    Added seed dev dep for seeding the sandbox.

---
+++ amplify/seed/seed.ts
@@ -0,0 +1,57 @@
+import { readFile } from "node:fs/promises";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import { Schema } from "../data/resource";
+import {
+  addToUserGroup,
+  createAndSignUpUser,
+  getSecret,
+  signInUser,
+} from "@aws-amplify/seed";
+import * as auth from "aws-amplify/auth";
+
+// this is used to get the amplify_outputs.json file as the file will not exist until sandbox is created
+const url = new URL("../../amplify_outputs.json", import.meta.url);
+const outputs = JSON.parse(await readFile(url, { encoding: "utf8" }));
+Amplify.configure(outputs);
+const dataClient = generateClient<Schema>();
+
+const username = await getSecret("username");
+const password = await getSecret("password");
+
+await signInUser({
+  username: username,
+  password: password,
+  signInFlow: "Password",
+});
+
+const response = await dataClient.models.Item.create(
+  {
+    name: 'Rubber Duck', 
+    price: 5, 
+    shopfront: 'Test Emporium',
+    health: 100, 
+    rarity: 0, 
+    image: 'rubberduck'
+  },
+  {
+    authMode: "userPool",
+  }
+);
+
+const response2 = await dataClient.models.Pet.create(
+  {
+    name: 'N', 
+    species: 'Human',
+    hunger: 0,
+    mood: 1,
+    owner: 'nnneato', 
+    health: 100, 
+    image: '4'
+  },
+  {
+    authMode: "userPool",
+  }
+);
+
+auth.signOut();
