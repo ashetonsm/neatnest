10d4cf967ea344dd37f4db62f5981572621ba6ae
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-09-07 22:33:08 -0500

    Added users schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -21,26 +21,42 @@ const schema = a.schema({
       // Users in the admin group have full permissions
       allow.groups(['admin'])
     ]),
-    Pet: a
-      .model({
-        name: a.string(),
-        species: a.string(),
-        hunger: a.integer(),
-        mood: a.integer(),
-        owner: a.string(),
-        health: a.integer(),
-        image: a.string()
-      })
-      .authorization((allow) => [
-        // Guests are read only
-        allow.guest().to(['read']),
-        // Authenticated users can read and write (change item values)
-        allow.authenticated('identityPool').to(['read', 'update']),
-        // Owners can read, write, and delete (change and consume items)
-        allow.owner().to(['read', 'update', 'delete']),
-        // Users in the admin group have full permissions
-        allow.groups(['admin'])
-      ]),
+  Pet: a
+    .model({
+      name: a.string(),
+      species: a.string(),
+      hunger: a.integer(),
+      mood: a.integer(),
+      owner: a.string(),
+      health: a.integer(),
+      image: a.string()
+    })
+    .authorization((allow) => [
+      // Guests are read only
+      allow.guest().to(['read']),
+      // Authenticated users can read and write (change item values)
+      allow.authenticated('identityPool').to(['read', 'update']),
+      // Owners can read, write, and delete (change and consume items)
+      allow.owner().to(['read', 'update', 'delete']),
+      // Users in the admin group have full permissions
+      allow.groups(['admin'])
+    ]),
+  User: a
+    .model({
+      username: a.string(),
+      itemsRemaining: a.integer(),
+      petsRemaining: a.integer()
+    })
+    .authorization((allow) => [
+      // Guests are read only
+      allow.guest().to(['read']),
+      // Authenticated users can read and write (change item values)
+      allow.authenticated('identityPool').to(['read', 'update']),
+      // Owners can read, write, and delete (change and consume items)
+      allow.owner().to(['read', 'update', 'delete']),
+      // Users in the admin group have full permissions
+      allow.groups(['admin'])
+    ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
