7759bed7dcc6fad9b08ee090fba4c55edd66e645
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-10-13 14:53:04 +0900

    Adds item creation button.

--- src/components/InventoryDisplay.vue
+++ src/components/InventoryDisplay.vue
@@ -11,12 +11,14 @@ const client = generateClient<Schema>()
 // create a reactive reference to Item[]
 const fetchedItems = ref<Array<Schema['Item']['type']>>([]);
 
+const canCreate = true
+
 async function fetchItems() { 
-  const cachedItems = localStorage.getItem('inventory')
-  if (cachedItems) {
-    console.log("Cached inventory found.")
-    fetchedItems.value = JSON.parse(cachedItems)
-  } else {
+  // const cachedItems = localStorage.getItem('inventory')
+  // if (cachedItems) {
+    // console.log("Cached inventory found.")
+    // fetchedItems.value = JSON.parse(cachedItems)
+  // } else {
     console.log("No cached inventory found, querying database.")
       const { signInDetails } = await getCurrentUser()
     const { data: items, errors } = await client.models.Item.listItemsByOwnerAndName(
@@ -29,7 +31,24 @@ async function fetchItems() {
     );
     localStorage.setItem('inventory', JSON.stringify(items))
     fetchedItems.value = items
-  }
+  // }
+}
+
+async function createItem() {
+  const { signInDetails } = await getCurrentUser()
+
+  client.models.Item.create({
+    name: "EMPORIUM ITEM",
+    price: 1,
+    shopfront: "Test Emporium",
+    // owner: signInDetails?.loginId ?? 'undefined',
+    owner: 'NA',
+    health: 1,
+    rarity: 1,
+    image: "a.string()"
+  }).then((res) => {
+    console.log(res)
+  });
 }
 
 onMounted(() => {
@@ -45,6 +64,9 @@ onMounted(() => {
       <p>This is where you view your inventory.</p>
     </div>
   <div class="page" id="inventoryPage">
+    <template v-if="canCreate">
+      <button @click="createItem">Create a new ITEM</button>
+    </template>
     <template v-if="!fetchedItems">
       <h1>Loading!</h1>
     </template>
