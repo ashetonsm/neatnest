0c5f8830505dcab7ad84cd75748cf65fefaa7ca9
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-10-26 12:40:53 +0900

    Adds permission to read, write, and delete s3 bucket objects.

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'neatNestImageStorage'
+  name: 'neatNestImageStorage',
+  access: (allow) => ({
+  'images/*': [
+    allow.guest.to(['read']),
+    allow.authenticated.to(['read', 'write', 'delete'])
+  ],
+})
 });
