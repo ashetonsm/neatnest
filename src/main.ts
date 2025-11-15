5832f2f724fd8cb97bf33c2b3bf8f1e34d321229
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-15 19:13:11 +0900

    Installed Pinia for global state management.

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import outputs from '../amplify_outputs.json'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 Amplify.configure(outputs)
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
