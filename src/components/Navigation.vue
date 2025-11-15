b25e5f099f253f25adf5e3a3a9b47586ec009ec2
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-15 19:13:56 +0900

    Added a little store check

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { userStore } from "@/stores/user";
+import { onMounted } from "vue";
 import { RouterLink } from "vue-router";
 
 var links = [
@@ -10,6 +12,14 @@ var links = [
   { title: "Pets", name: "/pets" },
   { title: "About", name: "/about" },
 ];
+
+const store = userStore();
+
+onMounted(async () => {
+  await store.amplifyGetCurrentUser().then(() => {
+    console.log(JSON.parse(JSON.stringify(store.getUser)));
+  });
+});
 </script>
 
 <template>
