ba79e57a8643e245fd5c01e9bdcf6bff602489a4
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-15 23:52:51 +0900

    Changes the profile link to the current user's username once it's loaded from the store.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,46 @@
 <script setup lang="ts">
 import { userStore } from "@/stores/user";
+import { onMounted, ref } from "vue";
 import { RouterLink } from "vue-router";
 
-var links = [
+const myProfile = ref("");
+
+const links = ref<Array<{ title: string; name: string }>>([
   { title: "Home", name: "/" },
   { title: "Shop 1", name: "/shop/1" },
   { title: "Shop 2", name: "/shop/2" },
   { title: "Inventory", name: "/inventory" },
-  { title: "Profile", name: "/profile" },
+  { title: "Profile", name: `/profile/null` },
   { title: "Pets", name: "/pets" },
   { title: "About", name: "/about" },
-];
+]);
 
 const store = userStore();
 
+onMounted(async () => {
+  store.$subscribe((mutation, state) => {
+    // Perform actions here when the state changes
+
+    if (mutation.storeId == "user") {
+      const un = store.getUser.username;
+      myProfile.value = un.toString();
+      console.log("un: ", un);
+      console.log("myProfile: ", myProfile.value);
+
+      links.value = [
+        { title: "Home", name: "/" },
+        { title: "Shop 1", name: "/shop/1" },
+        { title: "Shop 2", name: "/shop/2" },
+        { title: "Inventory", name: "/inventory" },
+        { title: "Profile", name: `/profile/${myProfile.value}` },
+        { title: "Pets", name: "/pets" },
+        { title: "About", name: "/about" },
+      ];
+    }
+    // console.log('Mutation:', mutation);
+    // console.log('New state:', state);
+  });
+});
 </script>
 
 <template>
