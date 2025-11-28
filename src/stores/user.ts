584db850b18b81ccb5457eef57a62deb4f2ffa52
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-28 19:02:07 +0900

    Added new states, getters, and actions for pets, invenory, and friends. Friends not yet implemented.

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,13 +3,20 @@ import { defineStore } from 'pinia';
 import { generateClient } from 'aws-amplify/api';
 import type { Schema } from '../../amplify/data/resource'
 import { getCurrentUser } from 'aws-amplify/auth';
+import { fetchInventory } from '@/components/tools/fetchInventory';
 
 export const userStore = defineStore('user', {
     state: () => ({
-        user: ref<any>(null)
+        user: ref<any>(null),
+        pets: ref<any>(null),
+        inventory: ref<any>(null),
+        friends: ref<any>(null),
     }),
     getters: {
         getUser: (state: { user: any }) => state.user,
+        getPets: (state: { pets: any }) => state.pets,
+        getInventory: (state: { inventory: any }) => state.inventory,
+        getFriends: (state: { friends: any }) => state.friends,
     },
     actions: {
         async amplifyGetCurrentUser() {
@@ -29,6 +36,46 @@ export const userStore = defineStore('user', {
             } catch (error: any) {
                 console.error(error)
             }
+        },
+
+        async fetchPets() {
+            const client = generateClient<Schema>();
+            try {
+                await client.models.Pet.listPetsByOwnerAndName(
+                    { owner: this.user.id, },
+                    { authMode: "userPool", })
+                    .then((res) => {
+                        this.pets = res.data
+                    })
+            } catch (error: any) {
+                console.error(error)
+            }
+        },
+
+        async fetchInventory() {
+            const client = generateClient<Schema>();
+
+            await client.models.Item.listItemsByOwnerAndName(
+                {
+                    owner: this.user
+                },
+                {
+                    headers: {
+                        'Access-Control-Allow-Headers': '*',
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': '*',
+                        'Content-Type': 'application/json',
+                    },
+                    authMode: 'userPool'
+                }
+            )
+                .then((res: { data: any; }) => {
+                    return this.inventory = res.data
+                })
+                .catch((error: any) => {
+                    console.log("No items found for this user.")
+                    return this.inventory = []
+                });
         }
     }
 });
