25ae8eb3122b6a4007bed7927e9bf64a8c5bfa64
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-30 20:50:35 +0900

    Replaces inventory and pet fetch with store value.

--- src/components/InventoryDisplay.vue
+++ src/components/InventoryDisplay.vue
@@ -2,14 +2,13 @@
 import Item from "./Item.vue";
 import { onMounted, ref } from "vue";
 import type { Schema } from "../../amplify/data/resource";
-import { fetchInventory } from "./tools/fetchInventory";
 import { userStore } from "@/stores/user";
 
 // These should be items where the owner is the logged in user
 const store = userStore();
 
 // create a reactive reference to Item[]
-var fetchedItems = ref<Array<Schema["Item"]["type"]>>([]);
+const fetchedItems: any = ref<Array<Schema["Item"]["type"]>>([]);
 var canCreate = true;
 
 async function setCreation() {
@@ -20,7 +19,7 @@ async function setCreation() {
 
 onMounted(async () => {
   await setCreation();
-  fetchedItems = await fetchInventory(store.getUser.id, fetchedItems);
+  fetchedItems.value = JSON.parse(JSON.stringify(store.getInventory));
 });
 </script>
 
@@ -30,24 +29,19 @@ onMounted(async () => {
     <p>This is where you view your inventory.</p>
   </div>
   <div class="page" id="inventoryPage">
-    <template v-if="canCreate">
-      <div>
-        <a href="/canvas/item" target="_blank">Launch Item Canvas</a>
-      </div>
-    </template>
-    <template v-if="!fetchedItems">
-      <h1>Loading!</h1>
-    </template>
-    <template v-else-if="!fetchedItems.length">
-      <h1>Your inventory is empty</h1>
-    </template>
-    <template v-else>
+    <div v-if="canCreate">
+      <a href="/canvas/item" target="_blank">Launch Item Canvas</a>
+    </div>
+    <div v-if="fetchedItems.length !== 0">
       <Item
         v-for="(item, i) in fetchedItems"
         :key="item.name ?? i"
         :item="item"
         :currentUser="store.getUser.id"
       />
-    </template>
+    </div>
+  </div>
+  <div v-if="fetchedItems.length == 0">
+    <h1>Your inventory is empty</h1>
   </div>
 </template>
