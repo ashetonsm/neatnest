5ba81c6c27c52be5f0d6a4b32db1b372e33dcfb2
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-12-27 15:08:05 +0900

    Stylizes the homepage.

--- src/components/HomeDisplay.vue
+++ src/components/HomeDisplay.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="page-header">
-    <h1>Home page</h1>
-    <p>This is where the latest news would be.</p>
-  </div>
-  <div class="page" id="homePage"></div>
+  <v-sheet
+    class="d-flex align-center justify-center text-center mx-auto pa-8"
+    elevation="4"
+    width="100%"
+    rounded
+  >
+    <v-row>
+      <v-col md="12" class="text-center">
+        <h2 class="text-h4 font-weight-black ma-4">Home</h2>
+        <p>This is where the latest news would be.</p>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
