789e2c676e4387351acbdf2d7e5a7b953fa1f790
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-12-27 15:08:21 +0900

    Stylized the about page with placeholder info.

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-sheet
+    class="d-flex align-center justify-center text-center mx-auto pa-8"
+    elevation="4"
+    width="100%"
+    rounded
+  >
+    <v-row>
+      <v-col md="12" class="text-center">
+        <h2 class="text-h4 font-weight-black ma-4">About NeatNest</h2>
+        <p>NeatNest was made by nnneato (AKA AshetonSM). That's pretty neat.</p>
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
