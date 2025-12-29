74a40a229117584278ff8927a00f9d4f2a359d51
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-12-29 12:45:34 +0900

    Added get and fetch for Credits. Untested fetch for friends.

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -9,12 +9,14 @@ export const userStore = defineStore('user', {
         user: ref<any>(null),
         pets: ref<any>(null),
         inventory: ref<any>(null),
+        credits: ref<any>(null),
         friends: ref<any>(null),
     }),
     getters: {
         getUser: (state: { user: any }) => state.user,
         getPets: (state: { pets: any }) => state.pets,
         getInventory: (state: { inventory: any }) => state.inventory,
+        getCredits: (state: { credits: any }) => state.credits,
         getFriends: (state: { friends: any }) => state.friends,
     },
     actions: {
@@ -76,6 +78,54 @@ export const userStore = defineStore('user', {
                 .catch((error: any) => {
                     console.log("No items found for this user.")
                     return this.inventory = []
+                });
+        },
+
+        async fetchCredit() {
+            const client = generateClient<Schema>();
+            await this.amplifyGetCurrentUser()
+
+            await client.models.Credit.cashByOwner(
+                {owner: this.user.id},
+                {authMode: 'userPool'}
+            )
+                .then(async (res: { data: any; }) => {
+                    if (res.data.length) {
+                        console.log("Found existing Credit.")
+                        return this.credits = res.data[0]
+                    } else {
+                    console.log("No Credit found for this user. Creating an entry...")
+                    await client.models.Credit.create(
+                        {
+                            owner: this.user.id,
+                            amount: 0
+                        }
+                    )
+                    .then((res: { data: any; }) => {
+                        return this.credits = res.data
+                    })
+                }
+                })
+                .catch(async (error: any) => {
+                    console.error("Oops, something went wrong!")
+                    console.error("Error: ", error)
+                });
+            },
+
+        async fetchFriends() {
+            const client = generateClient<Schema>();
+            await this.amplifyGetCurrentUser()
+
+            await client.models.Friend.friendByfriendA(
+                {friendA: this.user.id},
+                {authMode: 'userPool'}
+            )
+                .then((res: { data: any; }) => {
+                    return this.friends = res.data
+                })
+                .catch((error: any) => {
+                    console.log("No friends found for this user.")
+                    return this.friends = null
                 });
         }
     }
