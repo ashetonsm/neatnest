8fc585779204163d84a663474d1a5010411411ec
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-01-10 12:12:41 +0900

    Renamed auth to isAuthenticated.

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -6,11 +6,11 @@ import { userStore } from './user';
 
 export const authStore = defineStore('auth', {
     state: () => ({
-        auth: ref<Boolean>(false),
+        isAuthenticated: ref<Boolean>(false),
         userId: ref<String | null>(null)
     }), 
     getters: {
-        getAuth: (state: { auth: Boolean }) => state.auth,
+        getAuth: (state: { isAuthenticated: Boolean }) => state.isAuthenticated,
         getUserId: (state: { userId: String | null }) => state.userId
     },
     actions: {
@@ -18,12 +18,12 @@ export const authStore = defineStore('auth', {
             try {
                 const { userId } = await getCurrentUser();
                 this.userId = userId
-                this.auth = true;
+                this.isAuthenticated = true;
                 const user = userStore()
                 await user.fetchUser()
                 return true
             } catch (error: any) {
-                this.auth = false;
+                this.isAuthenticated = false;
                 return false
                 // console.error(error)
             }
