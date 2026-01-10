fd3160e2084694293b3666e6b4f579e2c70d5529
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-01-10 11:02:48 +0900

    Updates the routing so it uses fewer requests and checks less often for auth if it has already done so.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { Amplify } from 'aws-amplify'
 import outputs from '../amplify_outputs.json'
-import { createApp, inject } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
@@ -10,6 +10,7 @@ import { userStore } from './stores/user'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { authStore } from './stores/auth'
 
 Amplify.configure(outputs)
 const pinia = createPinia()
@@ -21,25 +22,47 @@ app.use(pinia)
 app.use(vuetify)
 app.mount('#app')
 
-// router.ts or main.ts
-router.beforeEach(async (to, from) => {
-    if (to.name !== "login") {
-        const store = userStore()
-        var user = store.getUser
-        var pets = store.getPets
-        var inventory = store.getInventory
-        if (user == null) {
-            await store.amplifyGetCurrentUser()
-            .then(() => {
-                user = store.getUser
-            })
-            
-            if (pets == null) {
-                pets = await store.fetchPets()
-            }
-            if (inventory == null) {
-                inventory = await store.fetchInventory()
+const auth = authStore();
+const store = userStore();
+
+router.beforeEach(async (to) => {
+    // Check for auth once
+    if (auth.getUserId == null && ["login", "about", "home"].includes(to.name as string)) {
+        console.log("no auth required")
+        return
+    } else {
+        console.log("auth required")
+        const authenticated = await auth.checkAuth()
+        if (authenticated) {
+            switch (to.name) {
+                case "inventory":
+                    await store.fetchInventory()
+                    return
+                case "store":
+                    await store.fetchCredit()
+                    return
+                case "profile":
+                    if (!store.getPets) {
+                        await store.fetchPets()
+                    } 
+                    if (!store.getFriends) {
+                        await store.fetchFriends(auth.getUserId as string)
+                    } 
+                    return
+                case "pets":
+                    if (!store.getPets) {
+                        await store.fetchPets()
+                    }
+                    if (!store.getInventory) {
+                        await store.fetchInventory()
+                    }
+                    return
+                default:
+                    console.log("Default switch")
+                    // Return the page (no extra prep is required)
+                    return
             }
         }
     }
-})
+}
+)
