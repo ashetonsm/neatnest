f2e8718d5af2f04a77cc1232c7500d903306ecdb
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-01-22 11:06:12 +0900

    Adds functionality to handleTrade. Stopped to rename owner to ownerId as the field was readonly and unassignable.

--- src/components/Trade.vue
+++ src/components/Trade.vue
@@ -2,10 +2,15 @@
 import { onMounted, ref } from "vue";
 import type { Schema } from "../../amplify/data/resource";
 import { userStore } from "@/stores/user";
+import { generateClient } from "aws-amplify/api";
 
+const client = generateClient<Schema>();
 const petData = ref<Schema["Pet"]["type"] | any>("");
 const user = userStore();
-const thisFriend = ref<{username: string, friendObject: Schema["Friend"]["type"]} | null>(null);
+const thisFriend = ref<{
+  username: string;
+  friendObject: Schema["Friend"]["type"];
+} | null>(null);
 
 const props = defineProps<{
   trade: Schema["Trade"]["type"];
@@ -15,35 +20,56 @@ const props = defineProps<{
 onMounted(async () => {
   const friendList = user.getFriends;
   // Get trader usernames
-  friendList.filter(
-    (f: any) => {
-      console.log(f.friendObject.friendB, props.trade.recipient)
-      console.log(f.friendObject.friendA, props.trade.recipient)
-        f.friendObject.friendB as string == props.trade.recipient as string
-    }
-  );
-  thisFriend.value = friendList[0]
+  friendList.filter((f: any) => {
+    console.log(f.friendObject.friendB, props.trade.recipient);
+    console.log(f.friendObject.friendA, props.trade.recipient);
+    (f.friendObject.friendB as string) == (props.trade.recipient as string);
+  });
+  thisFriend.value = friendList[0];
   petData.value = (await JSON.parse(props.pet)) as Schema["Pet"]["type"];
 });
 
 async function handleTrade(t: Schema["Trade"]["type"], action: string) {
   const choice = confirm(`${action} this trade?`);
   if (choice) {
-    switch (action) {
-      case "accept":
-        console.log("Accept trade.");
-        console.log(t);
-        break;
-      case "reject":
-        console.log("Reject trade.");
-        console.log(t);
-        break;
-      default:
-        console.log("Invalid trade action.");
-        break;
+    try {
+      var updatedPet = props.pet;
+      var updatedTrade = t;
+      switch (action) {
+        case "accept":
+          console.log("Accept trade.");
+          // Reassign the pet's owner
+          updatedPet.ownerId = user.getUser?.id;
+          await client.models.Pet.update(updatedPet)
+            .then((res: any) => {
+              console.log(res.data);
+            })
+            .then(() => {
+              updatedTrade.status = "completed";
+            });
+          console.log(t);
+          break;
+        case "reject":
+          console.log("Reject trade.");
+          updatedTrade.status = "rejected";
+
+          console.log(t);
+          break;
+        default:
+          console.log("Invalid trade action.");
+          break;
+      }
+
+      // Set trade status
+      await client.models.Trade.update(updatedTrade).then((res: any) => {
+        console.log(res.data);
+      });
+
+      // Refresh
+      // router.go(0);
+    } catch (error: any) {
+      console.error(error);
     }
-    // Refresh
-    // router.go(0);
   } else {
     return console.log(`Trade aborted.`);
   }
@@ -69,6 +95,7 @@ async function handleTrade(t: Schema["Trade"]["type"], action: string) {
     <!-- <v-card-actions v-if="trade.owner == user.getUser?.id && route.name == 'trades'"> -->
     <v-btn
       @click="handleTrade(props.trade, 'accept')"
+      :disabled="['accepted', 'rejected'].includes(props.trade.status!.toString())"
       text="Accept"
       class="mx-auto"
       variant="elevated"
@@ -76,6 +103,7 @@ async function handleTrade(t: Schema["Trade"]["type"], action: string) {
     ></v-btn>
     <v-btn
       @click="handleTrade(props.trade, 'reject')"
+      :disabled="['accepted', 'rejected'].includes(props.trade.status!.toString())"
       text="Reject"
       class="mx-auto"
       variant="elevated"
