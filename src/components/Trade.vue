6a0f5ed86508aaee18ccf03baf6382262945213e
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-01-22 10:40:43 +0900

    Displays traders' usernames for easier viewing.

--- src/components/Trade.vue
+++ src/components/Trade.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import type { Schema } from "../../amplify/data/resource";
+import { userStore } from "@/stores/user";
 
-const petData = ref<Schema["Pet"]["type"] | any>("")
+const petData = ref<Schema["Pet"]["type"] | any>("");
+const user = userStore();
+const thisFriend = ref<{username: string, friendObject: Schema["Friend"]["type"]} | null>(null);
 
 const props = defineProps<{
   trade: Schema["Trade"]["type"];
@@ -10,11 +13,18 @@ const props = defineProps<{
 }>();
 
 onMounted(async () => {
-  console.log(props.trade)
-  petData.value = await JSON.parse(props.pet) as Schema["Pet"]["type"]
-  console.log(petData.value)
-})
-
+  const friendList = user.getFriends;
+  // Get trader usernames
+  friendList.filter(
+    (f: any) => {
+      console.log(f.friendObject.friendB, props.trade.recipient)
+      console.log(f.friendObject.friendA, props.trade.recipient)
+        f.friendObject.friendB as string == props.trade.recipient as string
+    }
+  );
+  thisFriend.value = friendList[0]
+  petData.value = (await JSON.parse(props.pet)) as Schema["Pet"]["type"];
+});
 
 async function handleTrade(t: Schema["Trade"]["type"], action: string) {
   const choice = confirm(`${action} this trade?`);
@@ -43,24 +53,34 @@ async function handleTrade(t: Schema["Trade"]["type"], action: string) {
 <template>
   <v-card class="mx-auto" max-width="300px">
     <v-card-title class="text-center">{{ new Date(props.trade.createdAt) }}</v-card-title>
-    <v-card-subtitle>{{ `Trade between ${props.trade.sender} and ${props.trade.recipient}` }}</v-card-subtitle>
-    
-          <h3>Name: {{ petData.name }}</h3>
+    <v-card-subtitle>{{
+      `Trade between ${
+        props.trade.sender == user.getUser?.id
+          ? user.getUser?.username
+          : thisFriend?.username
+      } and ${
+        props.trade.recipient == user.getUser?.id
+          ? user.getUser?.username
+          : props.trade.recipient
+      }`
+    }}</v-card-subtitle>
+
+    <h3>Name: {{ petData.name }}</h3>
     <!-- <v-card-actions v-if="trade.owner == user.getUser?.id && route.name == 'trades'"> -->
-      <v-btn
-        @click="handleTrade(props.trade, 'accept')"
-        text="Accept"
-        class="mx-auto"
-        variant="elevated"
-        color="success"
-      ></v-btn>
-      <v-btn
-        @click="handleTrade(props.trade, 'reject')"
-        text="Reject"
-        class="mx-auto"
-        variant="elevated"
-        color="error"
-      ></v-btn>
+    <v-btn
+      @click="handleTrade(props.trade, 'accept')"
+      text="Accept"
+      class="mx-auto"
+      variant="elevated"
+      color="success"
+    ></v-btn>
+    <v-btn
+      @click="handleTrade(props.trade, 'reject')"
+      text="Reject"
+      class="mx-auto"
+      variant="elevated"
+      color="error"
+    ></v-btn>
     <!-- </v-card-actions> -->
   </v-card>
 </template>
