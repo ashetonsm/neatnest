0ad9b2192ee0abef9f8ffa9377bd593106976cd5
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-03-28 15:46:53 +0900

    Adds Auth0 login/logout functions to buttons.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
 import { RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
 import router from "@/router";
+import { useAuth0 } from "@auth0/auth0-vue";
+
+const { loginWithRedirect, logout: auth0Logout } = useAuth0();
+const logout = () =>
+  auth0Logout({ logoutParams: { returnTo: window.location.origin } })
+
 </script>
 
 <template>
@@ -11,10 +17,8 @@ import router from "@/router";
         <Navigation />
         <RouterView :key="$route.fullPath" />
       </v-container>
-      <v-btn>
-        Log Out
-      </v-btn>
-      <v-btn to="/login">Log in</v-btn>
+      <v-btn @click="logout"> Log Out </v-btn>
+      <v-btn @click="loginWithRedirect">Log in</v-btn>
     </v-main>
   </v-app>
 </template>
