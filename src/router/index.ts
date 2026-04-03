6c26a3b128d1fb35ac2f44ca18f264ad6758b4cf
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-03 15:17:32 +0900

    Uses the built in authguard feature to redirect certain routes to the login screen.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CanvasView from '@/views/CanvasView.vue'
+import { authGuard } from '@auth0/auth0-vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
@@ -19,6 +20,7 @@ const router = createRouter({
       path: '/callback',
       name: 'callback',
       component: () => import('@/views/CallbackView.vue'),
+      beforeEnter: authGuard
     },
     {
       path: '/login',
@@ -29,31 +31,37 @@ const router = createRouter({
       path: '/profile/:username',
       name: 'profile',
       component: () => import('@/views/ProfileView.vue'),
+      beforeEnter: authGuard
     },
     {
       path: '/trades',
       name: 'trades',
       component: () => import('@/views/TradesView.vue'),
+      beforeEnter: authGuard
     },
     {
       path: '/inventory',
       name: 'inventory',
       component: () => import('@/views/InventoryView.vue'),
+      beforeEnter: authGuard
     },
     {
       path: '/shop/:id',
       name: 'shop',
       component: () => import('@/views/ShopView.vue'),
+      beforeEnter: authGuard
     },
     {
       path: '/pets',
       name: 'pets',
       component: () => import('@/views/PetsView.vue'),
+      beforeEnter: authGuard
     },
     {
       path: '/canvas/:type',
       name: 'canvas',
       component: CanvasView,
+      beforeEnter: authGuard
     },
   ],
 })
