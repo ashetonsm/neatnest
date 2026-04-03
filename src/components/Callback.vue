3ea3de8092e316168d48eb75c2f71aba5c64b654
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-03 15:17:07 +0900

    Sets a cookie with the user's PK (currentUser) so that it can be retrieved when the page is refreshed.

--- src/components/Callback.vue
+++ src/components/Callback.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import router from "@/router";
 import { userStore } from "../stores/user";
 import { useAuth0 } from "@auth0/auth0-vue";
 
@@ -6,12 +7,23 @@ const uStore = userStore();
 const { user } = useAuth0();
 
 if (!uStore.getUser) {
-  await uStore.fetchUser(user.value?.sub as string, "#METADATA#", user).then(() => {
+  await uStore.fetchUser(user.value?.sub as string, "#METADATA", user).then(() => {
     console.log("Got the logged in uStore's Metadata in Callback: ", uStore.getUser);
+    setCookie(uStore.getUser.PK, 1)
+    router.push({name: 'pets'});
   });
 }
+
+function setCookie(cvalue: string, exdays: number) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = "currentUser=" + cvalue + ";" + expires + ";path=/";
+}
+
+
 </script>
 
 <template>
-  <h1>Setting the store...</h1>
+  <h1>Logging in...</h1>
 </template>
