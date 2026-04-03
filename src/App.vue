4de0565e46533db7d723d1ba2892b34a8520fe0b
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-03 15:16:04 +0900

    Clears cookie during logout.

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@ import router from "@/router";
 import { useAuth0 } from "@auth0/auth0-vue";
 
 const { loginWithRedirect, logout: auth0Logout } = useAuth0();
-const logout = () =>
+const logout = () => {
+  document.cookie = "currentUser=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; 
   auth0Logout({ logoutParams: { returnTo: window.location.origin } })
+}
 
 </script>
 
@@ -18,7 +20,7 @@ const logout = () =>
         <RouterView :key="$route.fullPath" />
       </v-container>
       <v-btn @click="logout"> Log Out </v-btn>
-      <v-btn @click="loginWithRedirect({appState: {target: '/callback'}})">Log in</v-btn>
+      <v-btn @click="loginWithRedirect({ appState: { target: '/callback' } })">Log in</v-btn>
     </v-main>
   </v-app>
 </template>
