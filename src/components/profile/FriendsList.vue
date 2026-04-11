8118a4f49386f3766a56b1812de73964660b61c4
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-11 10:26:12 +0900

    Placeholder component for displaying a friends list.

---
+++ src/components/profile/FriendsList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import router from "@/router";
+
+const props = defineProps<{
+  username: string;
+  friends: any[];
+}>();
+</script>
+
+<template>
+    <v-sheet border="md" class="pa-4 text-white mx-auto rounded" color="purple">
+        <h2 class="text-h4 font-weight-black ma-4">{{ username }}'s Friends:</h2>
+        <v-list v-if="
+            friends &&
+            friends.filter((f: { status: number; }) => f.status == 1).length !== 0
+        ">
+            <template v-for="n in friends.filter((f: { status: number; }) => f.status == 1)">
+                <v-list-item v-if="n" :key="'Friend: ' + n.username" :title="n.username.toString()"
+                    :to="'/profile/' + n.username"></v-list-item>
+            </template>
+        </v-list>
+        <div v-else>Aww, {{ username }} has no friends!</div>
+    </v-sheet>
+</template>
