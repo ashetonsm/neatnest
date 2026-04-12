716cc5a3c5af99ab7e39301ff747121b4384aa09
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-12 19:24:04 +0900

    Changed the type, might change again.

--- src/components/profile/FriendsList.vue
+++ src/components/profile/FriendsList.vue
@@ -2,19 +2,21 @@
 import router from "@/router";
 
 const props = defineProps<{
-  username: string;
-  friends: any[];
+    username: string;
+    friends: Record<string, any>[] | undefined;
 }>();
 </script>
 
+<!-- Right  now, this will print the user's own username,
+ so you should probably fix the username assignment later. -->
 <template>
     <v-sheet border="md" class="pa-4 text-white mx-auto rounded" color="purple">
         <h2 class="text-h4 font-weight-black ma-4">{{ username }}'s Friends:</h2>
         <v-list v-if="
             friends &&
-            friends.filter((f: { status: number; }) => f.status == 1).length !== 0
+            friends.filter((f: { status?: number; }) => f.status == 1).length !== 0
         ">
-            <template v-for="n in friends.filter((f: { status: number; }) => f.status == 1)">
+            <template v-for="n in friends.filter((f: { status?: number; }) => f.status == 1)">
                 <v-list-item v-if="n" :key="'Friend: ' + n.username" :title="n.username.toString()"
                     :to="'/profile/' + n.username"></v-list-item>
             </template>
