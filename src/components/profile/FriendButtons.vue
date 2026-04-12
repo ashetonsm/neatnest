e34cdc68be7514872745f550a3943f95d91c3649
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-12 19:23:36 +0900

    Created a simple component to control which buttons are displayed via an object prop.

---
+++ src/components/profile/FriendButtons.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import router from "@/router";
+import { onMounted, ref, toRaw } from "vue";
+
+const props = defineProps<{
+  buttonValues: {
+    add: boolean,
+    cancel: boolean,
+    remove: boolean,
+    accept: boolean,
+    reject: boolean,
+    block: boolean,
+    unblock: boolean
+  };
+}>();
+
+</script>
+
+<template>
+
+  <v-btn v-if="props.buttonValues.add">Add</v-btn>
+  <v-btn v-if="props.buttonValues.cancel">Cancel</v-btn>
+  <v-btn v-if="props.buttonValues.remove">Remove</v-btn>
+  <v-btn v-if="props.buttonValues.accept">Accept</v-btn>
+  <v-btn v-if="props.buttonValues.reject">Reject</v-btn>
+  <v-btn v-if="props.buttonValues.block">Block</v-btn>
+  <v-btn v-if="props.buttonValues.unblock">Unblock</v-btn>
+</template>
