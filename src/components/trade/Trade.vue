1f4ea6b807f5e48205bca34b66be1038147b9af8
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-18 18:15:33 +0900

    Updated to display more info, which will be split off into one or more independent components later.

--- src/components/trade/Trade.vue
+++ src/components/trade/Trade.vue
@@ -1,37 +1,74 @@
 <script setup lang="ts">
-import { shallowRef } from "vue";
+import { onMounted, ref, shallowRef, toRaw } from "vue";
 import { userStore } from "@/stores/user";
-
-const petData = shallowRef<any>(null);
-const thisFriend = shallowRef<any>(null);
-const user = userStore();
+import Pet from "../Pet.vue";
 
 const props = defineProps<{
   trade: any;
 }>();
 
+const textStatus = ref("")
+
 async function handleTrade(action: string) {
+  console.log(toRaw(props.trade.tradeContents[0]))
 }
+
+onMounted(() => {
+  console.log(props.trade.status)
+  switch (parseInt(props.trade.status)) {
+    case 0:
+      textStatus.value = "Waiting on You"
+      break;
+    case 1:
+      textStatus.value  = "Accepted"
+      break;
+    case 2:
+      textStatus.value  = "Rejected"
+      break;
+    case 8:
+      textStatus.value  = "Closed"
+      break;
+    case 9:
+      textStatus.value  = "Waiting on Them"
+      break;
+  
+    default:
+      textStatus.value  = "Invalid trade status!"
+      break;
+  } 
+})
 
 </script>
 
 <template>
   <v-card class="mx-auto" max-width="300px">
-    <v-card-title class="text-center">{{ new Date(props.trade.createdAt) }}</v-card-title>
+    <v-card-title class="text-center">{{ new Date(props.trade.createdAt).toUTCString() }}</v-card-title>
     <v-card-subtitle>{{
-      `Trade between ${
-        props.trade.sender == user.getUser?.id
-          ? user.getUser?.username
-          : thisFriend?.username
-      } and ${
-        props.trade.recipient == user.getUser?.id
-          ? user.getUser?.username
-          : props.trade.recipient
-      }`
+      `Trade between ${props.trade.username} and ${props.trade.tradeUsername}`
     }}</v-card-subtitle>
 
-    <h3>Pet Name: {{ petData?.name }}</h3>
-    <h3>Trade Status: {{ props.trade.status }}</h3>
+    <h3>Pet(s): {{ toRaw(props.trade.tradeContents[0]).length }}</h3>
+          <v-card 
+            class="mx-auto" max-width="200px"
+            v-for="(pet, i) in props.trade.tradeContents[0]"
+            :key="pet.name ?? i">
+            <v-card-title>{{ pet.name }}</v-card-title>
+            <v-card-subtitle>{{ pet.creator }}</v-card-subtitle>
+          </v-card>
+    <h3>Item(s): {{ toRaw(props.trade.tradeContents[1]).length }}</h3>
+          <v-card 
+            class="mx-auto" max-width="200px"
+            v-for="(item, i) in props.trade.tradeContents[1]"
+            :key="item.name ?? i">
+            <v-card-title>{{ item.name }}</v-card-title>
+            <v-card-subtitle>{{ item.creator }}</v-card-subtitle>
+          </v-card>
+    <h3>Credits:</h3>
+          <v-card 
+            class="mx-auto" max-width="200px">
+            <v-card-title>{{ toRaw(props.trade.tradeContents[2].credits) }}</v-card-title>
+          </v-card>
+    <h3>Trade Status: {{ textStatus }}</h3>
     <v-card-actions>
       <v-btn
         @click="handleTrade('accept')"
