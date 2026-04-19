b9242b5e180e0f4f9d3b315696f1e957b5ef06b1
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-19 19:22:31 +0900

    Added some styling.

--- src/components/profile/FriendButtons.vue
+++ src/components/profile/FriendButtons.vue
@@ -19,11 +19,60 @@ const props = defineProps<{
 
 <template>
 
-  <v-btn v-if="props.buttonValues.add" @click="updateFriend('add')">Add</v-btn>
-  <v-btn v-if="props.buttonValues.cancel" @click="updateFriend('remove')">Cancel</v-btn>
-  <v-btn v-if="props.buttonValues.remove" @click="updateFriend('remove')">Remove</v-btn>
-  <v-btn v-if="props.buttonValues.accept" @click="updateFriend('accept')">Accept</v-btn>
-  <v-btn v-if="props.buttonValues.reject" @click="updateFriend('remove')">Reject</v-btn>
-  <v-btn v-if="props.buttonValues.block" @click="updateFriend('block')">Block</v-btn>
-  <v-btn v-if="props.buttonValues.unblock" @click="updateFriend('remove')">Unblock</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.add" 
+    @click="updateFriend('add')"
+    text="Add"
+    class="mx-auto"
+    variant="elevated"
+    color="success"
+    >Add</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.cancel" 
+    @click="updateFriend('remove')"
+    text="Cancel"
+    class="mx-auto"
+    variant="elevated"
+    color="error"
+  >Cancel</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.remove" 
+    @click="updateFriend('remove')"
+    text="Remove"
+    class="mx-auto"
+    variant="elevated"
+    color="error"
+  >Remove</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.accept" 
+    @click="updateFriend('accept')"
+    text="Accept"
+    class="mx-auto"
+    variant="elevated"
+    color="success"
+  >Accept</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.reject" 
+    @click="updateFriend('remove')"
+    text="Reject"
+    class="mx-auto"
+    variant="elevated"
+    color="error"
+  >Reject</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.block" 
+    @click="updateFriend('block')"
+    text="Block"
+    class="mx-auto"
+    variant="elevated"
+    color="error"
+  >Block</v-btn>
+  <v-btn 
+    v-if="props.buttonValues.unblock" 
+    @click="updateFriend('remove')"
+    text="Unblock"
+    class="mx-auto"
+    variant="elevated"
+    color="error"
+  >Unblock</v-btn>
 </template>
