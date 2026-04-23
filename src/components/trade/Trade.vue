96d80c88b71ff8b7627c6b3c019a61e9d155b902
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-23 20:02:47 +0900

    Refreshes the page at the end of handleTrade. Fixes tradeContents.

--- src/components/trade/Trade.vue
+++ src/components/trade/Trade.vue
@@ -5,6 +5,7 @@ import { userStore } from "@/stores/user";
 import {
   UPDATE_TRADE
 } from "@/components/tools/ddbActions";
+import router from "@/router";
 
 const props = defineProps<{
   trade: any;
@@ -13,16 +14,21 @@ const props = defineProps<{
 const user = userStore();
 const textStatus = ref("")
 const buttonValues = ref({
-    cancel: false,
-    accept: false,
-    reject: false
-  })
+  cancel: false,
+  accept: false,
+  reject: false
+})
 
 async function handleTrade(action: string) {
-  var traderObj = {PK: '', tradeUsername: ''}
+  var traderObj = { PK: '', tradeUsername: '' }
   traderObj.PK = (props.trade.SK).match(/(?<=#)\S+/)[0]
   traderObj.tradeUsername = props.trade.tradeUsername
-  await UPDATE_TRADE(traderObj, user.getUser, props.trade.tradeContents[0], action)
+
+  await UPDATE_TRADE(traderObj, user.getUser, toRaw(props.trade.tradeContents), action)
+  .then(() => {
+      router.push({name: 'trades'})
+      router.go(0);
+  })
 }
 
 onMounted(() => {
@@ -46,11 +52,11 @@ onMounted(() => {
       textStatus.value = "Waiting on Them"
       buttonValues.value.cancel = true
       break;
-  
+
     default:
       textStatus.value = "Invalid trade status!"
       break;
-  } 
+  }
 })
 
 </script>
@@ -62,40 +68,31 @@ onMounted(() => {
       `Trade between ${props.trade.username} and ${props.trade.tradeUsername}`
     }}</v-card-subtitle>
 
-    <h3>Pet(s): {{ toRaw(props.trade.tradeContents[0]).length }}</h3>
-          <v-card 
-            v-if="toRaw(props.trade.tradeContents[0]).length !== 0"
-            class="mx-auto" max-width="200px"
-            v-for="(pet, i) in props.trade.tradeContents[0]"
-            :key="pet.name ?? i">
-            <v-card-title>{{ pet.name }}</v-card-title>
-            <v-card-subtitle>{{ pet.creator }}</v-card-subtitle>
-          </v-card>
-          <v-card v-else
-          class="mx-auto" max-width="200px">
-          <v-card-title>No pets</v-card-title>
-          </v-card>
+    <h3>Pet(s): {{ toRaw(props.trade.tradeContents[0].pets).length }}</h3>
+    <v-card v-if="toRaw(props.trade.tradeContents[0].pets).length !== 0" class="mx-auto" max-width="200px"
+      v-for="(pet, i) in props.trade.tradeContents[0].pets" :key="pet.name ?? i">
+      <v-card-title>{{ pet.name }}</v-card-title>
+      <v-card-subtitle>{{ pet.creator }}</v-card-subtitle>
+    </v-card>
+    <v-card v-else class="mx-auto" max-width="200px">
+      <v-card-title>No pets</v-card-title>
+    </v-card>
     <h3>Item(s):</h3>
-          <v-card 
-            v-if="toRaw(props.trade.tradeContents[1]).length > 0"
-            class="mx-auto" max-width="200px"
-            v-for="(item, i) in props.trade.tradeContents[1]"
-            :key="item.name ?? i">
-            <v-card-title>{{ item.name }}</v-card-title>
-            <v-card-subtitle>{{ item.creator }}</v-card-subtitle>
-          </v-card>
-          <v-card v-else
-          class="mx-auto" max-width="200px">
-            <v-card-title>No items</v-card-title>
-          </v-card>
+    <v-card v-if="toRaw(props.trade.tradeContents[1].items).length > 0" class="mx-auto" max-width="200px"
+      v-for="(item, i) in props.trade.tradeContents[1].items" :key="item.name ?? i">
+      <v-card-title>{{ item.name }}</v-card-title>
+      <v-card-subtitle>{{ item.creator }}</v-card-subtitle>
+    </v-card>
+    <v-card v-else class="mx-auto" max-width="200px">
+      <v-card-title>No items</v-card-title>
+    </v-card>
     <h3>Credits:</h3>
-          <v-card 
-            class="mx-auto" max-width="200px">
-            <v-card-title>{{ toRaw(props.trade.tradeContents[2].credits) }}</v-card-title>
-          </v-card>
+    <v-card class="mx-auto" max-width="200px">
+      <v-card-title>{{ toRaw(props.trade.tradeContents[2].credits) }}</v-card-title>
+    </v-card>
     <h3>Trade Status: {{ textStatus }}</h3>
     <v-card-actions>
-      <TradeButtons :buttonValues="buttonValues" :updateTrade="handleTrade"/>
+      <TradeButtons :buttonValues="buttonValues" :updateTrade="handleTrade" />
     </v-card-actions>
   </v-card>
 </template>
