723db0dbfd8f8c521779f0810233125ea763f0f4
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-04-23 20:05:06 +0900

    Mostly just the autoformatter messing things up. Added multple selections for pets and items.

--- src/components/trade/CreateTrade.vue
+++ src/components/trade/CreateTrade.vue
@@ -17,36 +17,35 @@ const pets = ref<Array<any>>(user.getPets);
 const items = ref<Array<any>>(user.getInventory);
 
 onMounted(async () => {
-  if (user.getFriends.length == 0) {
-    friends.value = await user.fetchFriends(user.getUser.PK) || []
-  }
-  if (user.getPets.length == 0) {
-    pets.value = await user.fetchPets(user.getUser.PK) || []
-  }
-  if (user.getInventory.length == 0) {
-    items.value = await user.fetchInventory() || []
-  }
+    if (user.getFriends.length == 0) {
+        friends.value = await user.fetchFriends(user.getUser.PK) || []
+    }
+    if (user.getPets.length == 0) {
+        pets.value = await user.fetchPets(user.getUser.PK) || []
+    }
+    if (user.getInventory.length == 0) {
+        items.value = await user.fetchInventory() || []
+    }
 })
 
 async function createTrade() {
     try {
         // The actual content of the trade
         const contents = [
-            {pets: toRaw(selectedPets.value)},
-            {items: toRaw(selectedItems.value)},
-            {credits: toRaw(selectedCredits.value)}
+            { pets: toRaw(selectedPets.value) },
+            { items: toRaw(selectedItems.value) },
+            { credits: toRaw(selectedCredits.value) }
         ]
-        console.log(contents)
-        var friendObj = {PK: '', tradeUsername: ''}
+        var friendObj = { PK: '', tradeUsername: '' }
         friendObj.PK = (selectedFriend.value.SK).match(/(?<=#)\S+/)[0]
         friendObj.tradeUsername = selectedFriend.value.relationshipUsername
         // The recipient, the sender, the contents, the action
         if (friendObj.PK !== undefined && friendObj.tradeUsername !== undefined) {
             await UPDATE_TRADE(friendObj, user.getUser, contents, 'create')
-            .then(() => {
-                router.push({name: 'trades'})
-                router.go(0);
-            })
+                .then(() => {
+                    router.push({ name: 'trades' })
+                    router.go(0);
+                })
         } else {
             console.error("Something is wrong with the data.", friendObj)
         }
@@ -74,47 +73,16 @@ async function validateTrade() {
             <v-expansion-panel-title><span>New Trade</span></v-expansion-panel-title>
             <v-expansion-panel-text>
                 <v-form @submit.prevent ref="tradeForm">
-                    <v-select
-                        v-model="selectedFriend"
-                        label="Friends"
-                        :items="friends"
-                        item-title="relationshipUsername"
-                        hint="Choose a Friend"
-                        item-value="friends"
-                        return-object
-                        single-line
-                        persistent-hint
-                    ></v-select>
-                    <v-select
-                        v-model="selectedPets"
-                        label="Pets"
-                        :items="pets"
-                        item-title="name"
-                        hint="Choose Pet(s)"
-                        item-value="pets"
-                        return-object
-                        single-line
-                        persistent-hint
-                    ></v-select>
-                    <v-select
-                        v-model="selectedItems"
-                        label="Items"
-                        :items="items"
-                        item-title="name"
-                        hint="Choose Item(s)"
-                        item-value="items"
-                        return-object
-                        single-line
-                        persistent-hint
-                    ></v-select>
-                    <v-number-input
-                        :v-model="selectedCredits"
-                        hint="Select Credit Amount"
-                        :max="999"
-                        :min="0"
-                        :step="1"
-                        persistent-hint
-                    ></v-number-input>
+                    <v-select v-model="selectedFriend" label="Friends" :items="friends"
+                        item-title="relationshipUsername" hint="Choose a Friend" item-value="friends" return-object
+                        single-line persistent-hint></v-select>
+                    <v-select v-model="selectedPets" label="Pets" :items="pets" item-title="name" hint="Choose Pet(s)"
+                        item-value="pets" return-object single-line persistent-hint multiple></v-select>
+                    <v-select v-model="selectedItems" label="Items" :items="items" item-title="name"
+                        hint="Choose Item(s)" item-value="items" return-object single-line persistent-hint
+                        multiple></v-select>
+                    <v-number-input :v-model="selectedCredits" hint="Select Credit Amount" :max="999" :min="0" :step="1"
+                        persistent-hint></v-number-input>
                     <v-btn class="mt-2" text="Submit" type="submit" @click.prevent="validateTrade"></v-btn>
                 </v-form>
             </v-expansion-panel-text>
