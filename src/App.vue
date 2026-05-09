2a29775ba0e3886f19ae71e5e0ae92d2349483bd
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-05-09 18:49:04 +0900

    Replaced with default template.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
-import Navigation from "./components/Navigation.vue";
-import router from "@/router";
-import { useAuth0 } from "@auth0/auth0-vue";
-
-const { loginWithRedirect, logout: auth0Logout } = useAuth0();
-const logout = () => {
-  document.cookie = "currentUser=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; 
-  auth0Logout({ logoutParams: { returnTo: window.location.origin } })
-}
-
+import Default from "./layouts/Default.vue";
 </script>
 
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <Navigation />
-        <RouterView :key="$route.fullPath" />
-      </v-container>
-      <v-btn @click="logout"> Log Out </v-btn>
-      <v-btn @click="loginWithRedirect({ appState: { target: '/callback' } })">Log in</v-btn>
-    </v-main>
-  </v-app>
+  <Default/>
 </template>
