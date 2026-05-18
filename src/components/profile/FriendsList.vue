82fc0d267b0ecb8103e87a89cb3459c21e1471b8
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-05-18 09:47:33 +0900

    Moves friend display into its own component. Has the blank display issue when a relationship exists but the user is blocked.

--- src/components/profile/FriendsList.vue
+++ src/components/profile/FriendsList.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Friend from './Friend.vue';
+
 
 const props = defineProps<{
     username: string;
@@ -6,18 +8,33 @@ const props = defineProps<{
 }>();
 </script>
 
+
+
 <template>
     <v-sheet border="md" class="pa-4 text-white mx-auto rounded" color="purple">
-        <h2 class="text-h4 font-weight-black ma-4">{{ username }}'s Friends:</h2>
-        <v-list v-if="
-            friends &&
-            friends.filter((f: { status?: number; }) => f.status == 1).length !== 0
-        ">
-            <template v-for="n in friends.filter((f: { status?: number; }) => f.status == 1)">
-                <v-list-item v-if="n" :key="'Friend: ' + n.relationshipUsername" :title="n.relationshipUsername.toString()"
-                    :to="'/profile/' + n.relationshipUsername"></v-list-item>
+        <h2 class="text-h4 font-weight-black ma-4">{{ username }}'s Relationships:</h2>
+        <!-- User has friends -->
+        <template v-if="props.friends!.length > 0">
+
+            <!-- The route is the friends page. List everyone regardless of status -->
+            <template v-if="$route.name == 'friends'">
+                <v-list v-for="friend in props.friends">
+                    <Friend :friend="friend"/>
+                </v-list>
             </template>
-        </v-list>
-        <div v-else>Aww, {{ username }} has no friends!</div>
+
+            <!-- The route is NOT the friends page. List only status 1 -->
+            <template v-else>
+                <v-list 
+                    v-for="friend in props.friends?.filter((f: { status?: number; }) => f.status == 1)">
+                    <Friend :friend="friend"/>
+                </v-list>
+            </template>
+        </template>
+
+        <!-- User has no friends -->
+        <template v-else>
+            <div>Aww, {{ username }} has no relationships!</div>
+        </template>
     </v-sheet>
 </template>
