2e6ccd4d2d03baacea97077d4fde4a2201bdebd5
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-05-25 20:25:10 +0900

    Removes the undefined type possiblity.

--- src/components/profile/FriendsList.vue
+++ src/components/profile/FriendsList.vue
@@ -4,7 +4,7 @@ import Friend from './Friend.vue';
 
 const props = defineProps<{
     username: string;
-    friends: Record<string, any>[] | undefined;
+    friends: Record<string, any>[];
 }>();
 </script>
 
@@ -14,7 +14,7 @@ const props = defineProps<{
     <v-sheet border="md" class="pa-4 text-white mx-auto rounded" color="purple">
         <h2 class="text-h4 font-weight-black ma-4">{{ username }}'s Relationships:</h2>
         <!-- User has friends -->
-        <template v-if="props.friends!.length > 0">
+        <template v-if="props.friends.length > 0">
 
             <!-- The route is the friends page. List everyone regardless of status -->
             <template v-if="$route.name == 'friends'">
@@ -26,7 +26,7 @@ const props = defineProps<{
             <!-- The route is NOT the friends page. List only status 1 -->
             <template v-else>
                 <v-list 
-                    v-for="friend in props.friends?.filter((f: { status?: number; }) => f.status == 1)">
+                    v-for="friend in props.friends.filter((f: { status?: number; }) => f.status == 1)">
                     <Friend :friend="friend"/>
                 </v-list>
             </template>
