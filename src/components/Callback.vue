2c317786df7b4b14f3818147b6cd390ff6f89527
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-05-25 20:23:31 +0900

    Updated to check for null and undefined username values.

--- src/components/Callback.vue
+++ src/components/Callback.vue
@@ -6,12 +6,13 @@ import { useAuth0 } from "@auth0/auth0-vue";
 const uStore = userStore();
 const { user } = useAuth0();
 
-if (!uStore.getUser) {
-  await uStore.fetchUser(user.value?.sub as string, "#METADATA", user).then(() => {
-    console.log("Got the logged in uStore's Metadata in Callback: ", uStore.getUser);
+if (!uStore.getUser || uStore.getUser.username == undefined) {
+  await uStore.fetchUser(user.value?.sub as string, "#METADATA", user)
+    .then(() => {
+    console.log("Got the logged in uStore's Metadata in Callback: ", uStore.getUser)
     setCookie(uStore.getUser.PK, 1)
-    router.push({name: 'pets'});
-  });
+    router.push({name: 'pets'})
+  })
 }
 
 function setCookie(cvalue: string, exdays: number) {
