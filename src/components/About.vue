70e0555ead0fd2c02cc2f272d5f9c4b0472db730
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-07-29 15:11:01 +0900

    Updated the placeholder text :P

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,8 +9,22 @@
   >
     <v-row>
       <v-col md="12" class="text-center">
-        <h2 class="text-h4 font-weight-black ma-4">About NeatNest</h2>
-        <p>NeatNest was made by nnneato (AKA AshetonSM). That's pretty neat.</p>
+        <h2 class="text-h4 font-weight-black ma-4">About Neatnest</h2>
+        <p>
+          Neatnest (or perhaps, nnneatopets...) was made by nnneato (AKA AshetonSM). That's pretty neat. 
+          Sometimes, you can find me in other places. 
+          You can even find me in Japan, where I currently live.
+        </p>
+        <v-row class="align-content-center">
+          <v-col>
+            <v-card variant="flat">
+              <a href="https://github.com/ashetonsm">
+                <v-avatar class="mb-4" image="https://dynamodb-misc-storage-files.s3.us-east-2.amazonaws.com/GitHub_Invertocat_Black.png"></v-avatar>
+              <p>Github</p>
+              </a>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-sheet>
--- src/components/HomeDisplay.vue
+++ src/components/HomeDisplay.vue
@@ -10,7 +10,10 @@
     <v-row>
       <v-col md="12" class="text-center">
         <h2 class="text-h4 font-weight-black ma-4">Home</h2>
-        <p>This is where the latest news would be.</p>
+        <p>
+          This is where the latest news would be if I'd managed 
+          to get that together before actually launching the site, but alas I did not...
+        </p>
       </v-col>
     </v-row>
   </v-sheet>
