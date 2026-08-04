5964b46f010065a19e46937862c280e0139896d8
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-08-04 13:14:37 +0900

    Experiments with loading a tumblr blog via a script.

--- src/components/HomeDisplay.vue
+++ src/components/HomeDisplay.vue
@@ -1,4 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+
+onMounted(() => {
+  getTumblrBlog()
+})
+
+const getTumblrBlog = () => {
+  const script = document.createElement("script")
+  script.type = "text/html"
+  script.src = "https://neatnest-news.tumblr.com/js"
+  // document.getElementById("newsfeed")?.appendChild(script)
+  document.body.appendChild(script)
+}
+
+</script>
 
 <template>
   <v-sheet
@@ -10,10 +26,10 @@
     <v-row>
       <v-col md="12" class="text-center">
         <h2 class="text-h4 font-weight-black ma-4">Home</h2>
-        <p>
+        <div id="newsfeed">
           This is where the latest news would be if I'd managed 
           to get that together before actually launching the site, but alas I did not...
-        </p>
+        </div>
       </v-col>
     </v-row>
   </v-sheet>
