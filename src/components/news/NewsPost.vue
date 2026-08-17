63949e8d2def1bb45caa0e091cb083e1cbc90c23
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-08-17 13:21:42 +0900

    Added a component to display the news with the date.

---
+++ src/components/news/NewsPost.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted, ref, shallowRef, toRaw } from "vue";
+
+const props = defineProps<{
+    post: any;
+}>();
+
+
+const reveal = ref(false)
+
+</script>
+
+<template>
+    <v-card width="100%">
+        <v-card-title class="text-center">{{new Date(props.post.date).toUTCString()}}</v-card-title>
+        <v-card-text v-html="props.post.body">
+        </v-card-text>
+    </v-card>
+</template>
