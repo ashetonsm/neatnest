37f63e06afc09360908169c4675526c683415145
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-08-21 20:01:49 +0900

    Updated to match createItem.

--- src/components/tools/createPet.ts
+++ src/components/tools/createPet.ts
@@ -13,49 +13,42 @@ export async function createPet(
 
   // Try uploading the image
   try {
-    canvas!.toBlob(async (blob) => {
-      try {
-        await UPLOAD_OBJECT(imgPath, blob)
-      } catch (error) {
-        console.error('Error : ', error);
-      }
-
-    }, 'image/png')
-
-    // Try creating a new Item
-    try {
-      await PUT_DATA({
-        PK: userObj.PK,
-        SK: `PET#${species}#${name}`,
-        creator: userObj.PK,
-        name: name,
-        owner: userObj.PK,
-        health: 100,
-        hunger: 5,
-        mood: 0,
-        status: 0,
-        url: imgPath,
-        type: 'Pet',
-        createdAt: new Date().getTime(),
-        updatedAt: new Date().getTime(),
-      })
-        .then(async () => {
-          // Update the user by decreasing itemsRemaining by 1 if itemsRemaining > 0
-          var updatedUser = userObj
-          // Subtract 1 from itemsRemaining
-          updatedUser.petsRemaining = updatedUser.petsRemaining - 1
-          // Update the updatedAt time for the User
-          updatedUser.updatedAt = new Date().getTime()
-          await PUT_DATA(updatedUser)
+    if (canvas) {
+      await UPLOAD_OBJECT(imgPath, canvas.toDataURL())
+        .then(async (res) => {
+          if (res.statusCode == 200) {
+            // Try creating a new Pet
+            await PUT_DATA({
+              PK: userObj.PK,
+              SK: `PET#${species}#${name}`,
+              creator: userObj.PK,
+              name: name,
+              owner: userObj.PK,
+              health: 100,
+              hunger: 5,
+              mood: 0,
+              status: 0,
+              url: imgPath,
+              type: 'Pet',
+              createdAt: new Date().getTime(),
+              updatedAt: new Date().getTime(),
+            })
+              .then(async () => {
+                // Update the user by decreasing petsRemaining by 1 if petsRemaining > 0
+                var updatedUser = userObj
+                // Subtract 1 from petsRemaining
+                updatedUser.petsRemaining = updatedUser.petsRemaining - 1
+                // Update the updatedAt time for the User
+                updatedUser.updatedAt = new Date().getTime()
+                await PUT_DATA(updatedUser)
+                  .then(() => {
+                    router.push({ name: 'pets' })
+                    router.go(1)
+                  })
+              });
+          }
         })
-        .then(() => {
-          router.push({ name: 'pets' })
-          router.go(1)
-        });
-    } catch (error: any) {
-      console.error(error)
     }
-
   } catch (e: any) {
     console.error("Error: ", e)
   }
