cc7678ef783ba6960d029403da0dd3c75c86ac52
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-09-05 22:04:20 +0900

    Fills the user store when they're already logged in before loading the route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import CanvasView from '@/views/CanvasView.vue'
-import { authGuard } from '@auth0/auth0-vue'
+import { authGuard, useAuth0 } from '@auth0/auth0-vue'
 import GamesView from '@/views/GamesView.vue'
 import GamesList from '@/components/games/GamesList.vue'
 import GamesDisplay from '@/components/games/GamesDisplay.vue'
+import { userStore } from '@/stores/user'
+import { toRaw } from 'vue'
 
 const router = createRouter({
   history: createWebHistory('/'),
@@ -13,84 +15,114 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: false }
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('@/views/AboutView.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/callback',
       name: 'callback',
       component: () => import('@/views/CallbackView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: false }
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/LoginView.vue'),
+      meta: { requiresAuth: false }
     },
     {
       path: '/profile/:username',
       name: 'profile',
       component: () => import('@/views/ProfileView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/trades',
       name: 'trades',
       component: () => import('@/views/TradesView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/friends',
       name: 'friends',
       component: () => import('@/views/FriendsView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/inventory',
       name: 'inventory',
       component: () => import('@/views/InventoryView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/shop/:id',
       name: 'shop',
       component: () => import('@/views/ShopView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/pets',
       name: 'pets',
       component: () => import('@/views/PetsView.vue'),
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/canvas/:type',
       name: 'canvas',
       component: CanvasView,
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/games',
       name: 'games',
       component: GamesView,
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/games',
       name: 'games',
       component: GamesList,
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
     {
       path: '/games/:id',
       name: 'gameDisplay',
       component: GamesDisplay,
-      beforeEnter: authGuard
+      beforeEnter: authGuard,
+      meta: { requiresAuth: true }
     },
   ],
+})
+
+router.beforeResolve(async to => {
+  const { user } = useAuth0();
+  const store = userStore()
+  const auth0 = useAuth0();
+  if (to.meta.requiresAuth && !auth0.isAuthenticated.value) {
+    return false
+  } else {
+    if (user.value) {
+      await store.fetchUser(user.value.sub as string, "#METADATA", toRaw(user.value))
+      .then(async () => {
+        return true
+      })
+    }
+  }
 })
 
 export default router
