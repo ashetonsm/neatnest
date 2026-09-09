b07da83c55a4e168096364765c8e01ea10269ab6
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-09-09 21:34:12 +0900

    Got rid of redundant user fetching.

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -2,14 +2,12 @@
 import { userStore } from '@/stores/user';
 import FriendsList from './profile/FriendsList.vue';
 import { onMounted, ref } from 'vue';
-import { useAuth0 } from '@auth0/auth0-vue';
 
 const store = userStore()
 const friends = ref()
 
 async function getFriends() {
     const data = await store.fetchRelationships(store.getUser.username, "", "")
-    console.log("Friends.vue data", data)
     // Do not return the data inside of an array, it's unnecessary.
     if (data) {
         return data
@@ -19,10 +17,6 @@ async function getFriends() {
 }
 
 onMounted(async () => {
-    const { user } = useAuth0();
-    if (store.getUser == null || store.getUser == undefined) {
-        await store.fetchUser(user.value?.sub as string, "%23METADATA")
-    }
     friends.value = await getFriends()
 })
 </script>
