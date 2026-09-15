00490c65df3e42717ac3941cd3b2d57cebe7475c
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-09-15 19:13:06 +0900

    Updated friends display but it only works for the Friends list page so far.

--- src/components/profile/FriendsList.vue
+++ src/components/profile/FriendsList.vue
@@ -22,14 +22,14 @@ onMounted(() => {
             <!-- The route is the friends page. List everyone regardless of status -->
             <template v-if="$route.name == 'friends'">
                 <v-list>
-                    <Friend :friend="props.friends"/>
+                    <Friend :friend="props.friends" :status="props.friends.status"/>
                 </v-list>
             </template>
 
             <!-- The route is NOT the friends page. List only status 1 -->
             <template v-else>
                 <v-list>
-                    <Friend v-if="props.friends.status == 1" :friend="props.friends"/>
+                    <Friend v-if="props.friends.status == 1" :friend="props.friends" :status="props.friends.status"/>
                 </v-list>
             </template>
         </template>
@@ -40,7 +40,7 @@ onMounted(() => {
             <!-- The route is the friends page. List everyone regardless of status -->
             <template v-if="$route.name == 'friends'">
                 <v-list v-for="friend in props.friends">
-                    <Friend :friend="friend"/>
+                    <Friend :friend="friend" :status="friend.status"/>
                 </v-list>
             </template>
 
@@ -48,7 +48,7 @@ onMounted(() => {
             <template v-else>
                 <v-list 
                     v-for="friend in props.friends">
-                    <Friend v-if="friend.status == 1" :friend="friend"/>
+                    <Friend v-if="friend.status == 1" :friend="friend" :status="friend.status"/>
                 </v-list>
             </template>
         </template>
