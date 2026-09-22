624493b6770ab580eac74a0a4631c195e14013bb
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2026-09-22 15:43:36 +0900

    Adds basic rules that weren't in use before for validation. Setup for getting from an endpoint, but should be changed to the lambda function url later.

--- src/components/profile/ChangeProfile.vue
+++ src/components/profile/ChangeProfile.vue
@@ -1,42 +1,67 @@
 <script setup lang="ts">
-import router from "@/router";
 import { userStore } from "@/stores/user";
-import { ref } from "vue";
-import { PUT_DATA } from "../tools/ddbActions";
+import { onMounted, ref } from "vue";
+import { useAuth0 } from "@auth0/auth0-vue";
 const store = userStore();
-const emailForm = ref()
-const bioForm = ref()
+const settingsForm = ref()
 const email = ref("")
 const bio = ref(store.getUser!.bio)
 
+const { getAccessTokenSilently } = useAuth0()
 
-async function validateEmail() {
+
+onMounted(async () => {
+    const res = await getToken()
+})
+
+async function getToken() {
     try {
-        const { valid } = await emailForm.value.validate()
-
-        if (valid) {
-
-
+        const options = {
+            authorizationParams:
+            {
+                redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+                audience: 'https://nnneato.com/dev-api/'
+            }
         }
+        const token = await getAccessTokenSilently(options);
+        console.log(token)
+        return fetch(`https://nnneato.com/dev-api/api/v2/users/${store.getUser.PK}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(async (response) => {
+                console.log(await response.text())
+                return response
+            })
+            .then((res) => {
+                console.log(res)
+            })
     } catch (error: any) {
         console.error(error);
     }
-}
+};
 
-async function validateBio() {
-    const { valid } = await bioForm.value.validate()
 
-    if (valid) {
-        var updatedUser = store.getUser!;
-        try {
-            updatedUser.bio = bio.value;
-            await PUT_DATA(updatedUser).then(async () => {
-                router.push(`/profile/${updatedUser.username}`)
-                router.go(0);
-            });
-        } catch (error: any) {
-            console.error(error);
+const emailRules = ref([
+    (v: string) => (v !== "") || 'Email cannot be blank.',
+    (v: string) => (/.+@.+\..+/.test(v)) || 'Invalid email.'
+])
+
+const bioRules = ref([
+    (v: string) => (v !== "") || 'Bio cannot be blank.'
+])
+
+async function validateChanges() {
+    try {
+        const { valid } = await settingsForm.value.validate()
+
+        if (valid) {
         }
+        console.log("settingsForm.value:", valid, settingsForm.value)
+
+    } catch (error: any) {
+        console.error(error);
     }
 }
 
@@ -46,27 +71,21 @@ async function validateBio() {
     <!-- Change your email address -->
     <v-expansion-panels>
         <v-expansion-panel>
-            <v-expansion-panel-title><span>Change email</span></v-expansion-panel-title>
+            <v-expansion-panel-title><span>Change User Settings</span></v-expansion-panel-title>
             <v-expansion-panel-text>
-                <v-form @submit.prevent ref="usernameForm">
-                    <v-text-field v-model="email" label="Email"
-                        placeholder="email@email.com"></v-text-field>
-                    <v-btn class="mt-2" text="Submit" type="submit" @click.prevent="validateEmail"></v-btn>
+                <v-form @submit.prevent ref="settingsForm">
+                    <v-text-field v-model="email" label="Email" placeholder="email@email.com"
+                        :rules="emailRules"></v-text-field>
+                    <v-btn class="mt-2" text="Submit" type="submit" @click.prevent="validateChanges"></v-btn>
+
+                    <!-- Change your description -->
+                    <v-text-field v-model="bio" label="Bio" :placeholder="store.getUser!.bio!"
+                        :rules="bioRules"></v-text-field>
+                    <v-btn class="mt-2" text="Submit" type="submit" @click.prevent="validateChanges"></v-btn>
+
                 </v-form>
             </v-expansion-panel-text>
         </v-expansion-panel>
     </v-expansion-panels>
 
-    <!-- Change your description -->
-    <v-expansion-panels>
-        <v-expansion-panel>
-            <v-expansion-panel-title><span>Change description</span></v-expansion-panel-title>
-            <v-expansion-panel-text>
-                <v-form @submit.prevent ref="bioForm">
-                    <v-text-field v-model="bio" label="Bio" :placeholder="store.getUser!.bio!"></v-text-field>
-                    <v-btn class="mt-2" text="Submit" type="submit" @click.prevent="validateBio"></v-btn>
-                </v-form>
-            </v-expansion-panel-text>
-        </v-expansion-panel>
-    </v-expansion-panels>
 </template>
